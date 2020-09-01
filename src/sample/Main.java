65251e29cf5703b5b4685a861af7852d6db4599c
Jon Eikholm <jone@kea.dk>
2020-09-01 10:52:36 +0200

    added button

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,9 +1,9 @@
 package sample;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.Background;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
@@ -12,6 +12,11 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         VBox vBox = new VBox();
+        Button button = new Button("press");
+        vBox.getChildren().addAll(button);
+        button.setOnAction(e -> {
+            button.setText("you rock!");
+        });
         primaryStage.setTitle("Hello World");
         primaryStage.setScene(new Scene(vBox, 300, 275));
         primaryStage.show();
