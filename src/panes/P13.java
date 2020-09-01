802580cc847f29cc5a56f54a7bb0d15ab42374eb
Peter Hartmann <iw4ffl3z@gmail.com>
2020-09-01 14:44:49 +0200

    Moo

--- src/panes/P13.java
+++ src/panes/P13.java
@@ -1,7 +1,5 @@
 package panes;
 
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
@@ -9,8 +7,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 
@@ -21,15 +19,14 @@ public class P13 extends Pane {
         FileInputStream inputstream = new FileInputStream("src/panes/cow.jpeg");
         Image cow = new Image(inputstream);
         ImageView imw = new ImageView(cow);
-//        imw.setX(50);
-//        imw.setY(200);
-//        imw.setFitHeight(166);
-//        imw.setFitWidth(240);
         Label label = new Label("P13");
         imw.setPreserveRatio(true);
         Group root = new Group(imw);
         Button button = new Button("Press Me!");
-        button.setOnAction(e -> label.setText("MOO!"));
+        button.setOnAction(e -> {
+            label.setText("MOO!");
+            label.setFont(new Font(100));
+        });
         box.getChildren().addAll(label, root, button);
         getChildren().addAll(box);
 
